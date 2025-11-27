1482df44cbae82a7f08c4dd6388b15f761220758
vincent malitine <vincent.malitine@saintmichelannecy.fr>
2025-11-27 10:54:36 +0100

    0.01

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Media;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.VisualBasic;
 
 namespace Wail_Vincent_AventCalender
 {
@@ -16,9 +18,41 @@ namespace Wail_Vincent_AventCalender
     /// </summary>
     public partial class MainWindow : Window
     {
+        SoundPlayer bgm = new SoundPlayer(@"BGM\bgm1.wav");
         public MainWindow()
         {
             InitializeComponent();
+
+            // Demande le prénom à l'utilisateur
+            var prenom = Interaction.InputBox(
+                "Entrez votre prénom :",
+                "Bienvenue",
+                ""
+            ).Trim();
+
+            var genre = Interaction.InputBox(
+                "Entrez votre genre :",
+                ""
+            ).Trim();
+
+            if (string.IsNullOrWhiteSpace(prenom))
+            {
+                MessageBox.Show("Bonjour !");
+            }
+            else
+            {
+                MessageBox.Show($"Bonjour {prenom} !");
+            }
+            // Lecture en boucle
+            bgm.PlayLooping();
+        }
+
+        protected override void OnClosed(System.EventArgs e)
+        {
+            // Arrête et libère la ressource audio à la fermeture
+            bgm.Stop();
+            bgm.Dispose();
+            base.OnClosed(e);
         }
     }
 }
