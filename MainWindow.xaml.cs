a847754b5c2f6304547f0172a49589220b47a4e4
vincent malitine <vincent.malitine@saintmichelannecy.fr>
2025-12-05 09:57:43 +0100

    0.15

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -140,7 +140,9 @@ namespace Wail_Vincent_AventCalender
 
         private void Button_TodayCard_Click(object sender, RoutedEventArgs e)
         {
-
+            CardContentHost.Children.Clear();
+            Card card = new Card();
+            CardContentHost.Children.Add(card);
         }
     }
 }
